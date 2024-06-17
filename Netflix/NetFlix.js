6c33e379a9c0016791bcabce5fb4f0434240306a
NaincyKingrani <167568259+NaincyKingrani@users.noreply.github.com>
2024-06-17 20:32:30 +0500

    Add files via upload

---
+++ Netflix/NetFlix.js
@@ -0,0 +1,16 @@
+
+
+function validation(){
+    
+
+
+    var data = document.getElementsByClassName("input").value;
+    if (data =="")
+        {
+            alert("input is missing");
+        }
+        else{
+            alert("input is  not missing");
+        }
+}
+    
